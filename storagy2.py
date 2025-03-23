c6cf09bfd112c4a7bee47d1e100757a98122c45b
Tobias Zicha <tobitv81@gmail.com>
2025-03-23 03:01:54 +0200

    4 new functions added array_min array_max most_expensive_product cheapest_product

--- storagy2.py
+++ storagy2.py
@@ -55,13 +55,49 @@ def add_product():
     print("Produkt úspěšně přidán")
 
 
+def array_min(items):
+    min_item = items[0]
+    for item in items[1:]:
+        if item < min_item:
+            min_item = item
+    return min_item
+
+def cheapest_product():
+
+    min_price = array_min([product["price"] for product in products])
+    cheapest = [product for product in products if product["price"] == min_price]
+
+    print("Nejlevnější produkt:")
+    for product in cheapest:
+        print(f"{product['name']} - {product['price']}$")
+
+def array_max(items):
+    max_item = items[0]
+    for item in items[1:]:
+        if item > max_item:
+            max_item = item
+    return max_item
+
+def most_expensive_product():
+
+    max_price = array_max([product["price"] for product in products])
+    most_expensive = [product for product in products if product["price"] == max_price]
+
+    print("Nejdražší produkt:")
+    for product in most_expensive:
+        print(f"{product['name']} - {product['price']}$")
+
+
+
 def menu():
     print("Vítej ve skladu")
     print("###############\n")
     print("1. Výpis položek")
     print("2. Přidání položky")
     print("3. Vyhledání produktu")
-    print("4. suma produktu\n")
+    print("4. suma produktu")
+    print("5. Nejdražší produkt")
+    print("6. Nejlevně produkt\n")
 
     choice = int(input("Volba: "))
 
@@ -86,6 +122,18 @@ def menu():
     elif choice == 4:
         print("suma produktu:")
         total_price()
+        print("")
+        menu()
+
+    elif choice == 5:
+        print("Nejdražší produkt:")
+        most_expensive_product()
+        print("")
+        menu()
+
+    elif choice == 6:
+        print("Nejlevnější produkt:")
+        cheapest_product()
         print("")
         menu()
 
