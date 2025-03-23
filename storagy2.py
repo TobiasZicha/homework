dac06590b598cac788cd2d2d340105075590db92
Tobias Zicha <tobitv81@gmail.com>
2025-03-23 14:41:44 +0200

    added array_min - cheapest_product       array_max - most_expensive_product

--- storagy2.py
+++ storagy2.py
@@ -55,13 +55,48 @@ def add_product():
     print("Produkt úspěšně přidán")
 
 
+def array_min(items):
+    min_item = items[0]
+    for item in items[1:]:
+        if item < min_item:
+            min_item = item
+    return min_item
+
+def cheapest_product():
+
+    min_price = array_min([product["price"] for product in products])
+    cheapest = [product for product in products if product["price"] == min_price]
+
+    print("Nejlevnější produkt:")
+    for product in cheapest:
+        print(f"{product['name']} - {product['price']}$")
+
+def array_max(items):
+    max_item = items[0]
+    for item in items[1:]:
+        if item > max_item:
+            max_item = item
+    return max_item
+
+def most_expensive_product():
+
+    max_price = array_max([product["price"] for product in products])
+    most_expensive = [product for product in products if product["price"] == max_price]
+
+    print("Nejdražší produkt:")
+    for product in most_expensive:
+        print(f"{product['name']} - {product['price']}$")
+
+
 def menu():
     print("Vítej ve skladu")
     print("###############\n")
     print("1. Výpis položek")
     print("2. Přidání položky")
     print("3. Vyhledání produktu")
-    print("4. Celková cena\n")
+    print("4. Celková cena")
+    print("5. Nejdražší produkt")
+    print("6. Nejlevnější produkt\n")
 
 
     choice = int(input("Volba: "))
@@ -69,21 +104,37 @@ def menu():
     if choice == 1:
         print("Položky na skladě jsou:")
         print_products()
+        print("")
         menu()
 
     elif choice == 2:
         print("Přidání poožky:")
         add_product()
+        print("")
         menu()
 
     elif choice == 3:
         print("vyhledání produktu:")
         search_product()
+        print("")
         menu()
 
     elif choice == 4:
-        print("suma produktu:")
+        print("Celková cena:")
         total_price()
+        print("")
+        menu()
+
+    elif choice == 5:
+        print("Nejdražší produkt:")
+        most_expensive_product()
+        print("")
+        menu()
+
+    elif choice == 6:
+        print("Nejlevnější produkt:")
+        cheapest_product()
+        print("")
         menu()
 
     else:
